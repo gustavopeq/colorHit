90472a7bd6986cde589a28484095411dc53654ff
gustavopeq <gustavo_pequeno2@hotmail.com>
2019-12-18 20:24:44 -0800

    Centered canvas created

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("Connected");
+var colorSquare;
+
+function startGame()
+{
+    gameArea.start();
+    colorSquare = new createColorSquare(50, 100, "red");
+}
+
+var gameArea = {
+    canvas : document.getElementById("canvas"),
+    start : function() {
+        this.canvas.width = 400;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+    },
+}
+
+function createColorSquare(posX, posY, color)
+{
+    context = gameArea.context;
+    context.fillStyle = color;
+    context.fillRect(posX, posY, 20, 20);
+}
