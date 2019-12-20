f7f876d689c547e079a9e307521d5a67a2236d53
gustavopeq <gustavo_pequeno2@hotmail.com>
2019-12-19 21:58:38 -0800

    Object Square created / Canvas update / square movement

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var colorSquare;
+var initialPosX = 50;
+var initialPosY = 100;
+var squareDefaultWidth = 20;
+var squareDefaultHeight = 20;
+var squareDefaultSpeed = 2;
+var squareDefaultColor = "red";
 
-function startGame()
-{
-    gameArea.start();
-    colorSquare = new createColorSquare(50, 100, "red");
-}
+var arrayOfSquares = [];
 
 var gameArea = {
     canvas : document.getElementById("canvas"),
@@ -12,12 +13,41 @@ var gameArea = {
         this.canvas.width = 400;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
+        this.interval = setInterval(updateGameArea, 16);
     },
+    clear : function()
+    {
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+    }
 }
 
-function createColorSquare(posX, posY, color)
+function startGame()
 {
-    context = gameArea.context;
-    context.fillStyle = color;
-    context.fillRect(posX, posY, 20, 20);
+    gameArea.start();
+
+    var newSquare = new Square(initialPosX,initialPosY,squareDefaultSpeed,squareDefaultColor);
+    newSquare.drawSquare();
+    arrayOfSquares.push(newSquare);
+}
+
+
+function updateGameArea()
+{
+    gameArea.clear();
+    arrayOfSquares[0].positionY+=1;
+    arrayOfSquares[0].drawSquare();
+}
+
+function Square(positionX, positionY, speed, color) 
+{
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.speed = speed;
+    this.color = color;
+
+    this.drawSquare = function()
+    {
+        gameArea.context.fillStyle = this.color;
+        gameArea.context.fillRect(this.positionX, this.positionY, squareDefaultWidth, squareDefaultHeight);
+    }
 }
